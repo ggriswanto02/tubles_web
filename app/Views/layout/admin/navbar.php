b4fa3668c89d49319275bc0fbe37554ae3e3f743
mamankirfa <irsyad.f2004@gmail.com>
2025-12-10 20:09:38 +0700

    update navbar korelasi_cpl_cpmk

--- app/Views/layout/admin/navbar.php
+++ app/Views/layout/admin/navbar.php
@@ -30,7 +30,7 @@
         <ul class="dropdown-menu bg-gray-100" aria-labelledby="tablesDropdown" >
           <li><a class="dropdown-item" href="<?= base_url('rpl') ?>">Capain Lulusan</a></li>
           <li><a class="dropdown-item" href="<?= base_url('rpl') ?>">Rencana Pembelajaran</a></li>
-          <li><a class="dropdown-item" href="<?= base_url('rpl') ?>">Korelasi CPL–CPMK</a></li>
+          <li><a class="dropdown-item" href="<?= base_url('table/korelasi-cpl-cpmk') ?>">Korelasi CPL–CPMK</a></li>
           <li><a class="dropdown-item" href="<?= base_url('rpl') ?>">Nilai Pertemuan</a></li>
         </ul>
       </li>
