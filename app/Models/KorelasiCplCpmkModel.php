24c8d676a3f53b7032be9272e7744e6d5957efae
mamankirfa <irsyad.f2004@gmail.com>
2025-12-18 23:58:58 +0700

    fix models

--- app/Models/KorelasiCplCpmkModel.php
+++ app/Models/KorelasiCplCpmkModel.php
@@ -6,9 +6,14 @@ use CodeIgniter\Model;
 
 class KorelasiCplCpmkModel extends Model
 {
-    protected $table = 'korelasi_cpl_cpmk';
-    protected $primaryKey = 'id';
-    protected $allowedFields = [
+    protected $table            = 'korelasi_cpl_cpmk';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+
+    protected $allowedFields    = [
         'id_penyusun',
         'id_matakuliah',
         'sub_cpmk',
@@ -17,5 +22,27 @@ class KorelasiCplCpmkModel extends Model
         'bobot_penilaian'
     ];
 
-    protected $useTimestamps = false;
+    // Dates
+    protected $useTimestamps = true; 
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
 }
